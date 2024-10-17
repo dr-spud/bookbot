c9c156be74a7d0b06e60acbfe8541755b9592fd0
dr-spud <xzhuc@gmail.com>
2024-10-17 11:54:46 +1100

    printed char counts

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     character_count = get_char_count(text)
-    
+    sorted_dict = get_sorted_dict(character_count)
     # print(text)
     print(f"---Begin report of {book_path}---")
     print(f"There are {word_count} words in {book_path}") 
 
-    print(character_count)
-    print(get_char_list(character_count))
+    # print(character_count)
+    for c in sorted_dict:   
+        char_key = c["char"]
+        count_key = c["count"]
+        print(f"The letter '{char_key}' has appeared '{count_key}' times")       
+
+    
 
     
 
@@ -37,13 +42,17 @@ def get_char_count(book):
     
     return char_dict
 
+
+
 def sort_on(dict):
     return dict['count']
 
-def get_char_list(dict):
+def get_sorted_dict(dict):
     char_list = [{'char': char, 'count': count} for char, count in dict.items()]
-    print(char_list)
+    char_list.sort(reverse=True, key=sort_on)
     return char_list
+
+
 
 
 
